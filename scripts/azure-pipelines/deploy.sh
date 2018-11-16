3f28bd240ddda79f740af80a23810d639679490d
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-16 12:14:04 +0100

    [WIP] Migrate macOS and iOS builds to Azure Pipelines
    Build time limits on Travis CI are becoming problematic.
    
    [ci skip]

---
+++ scripts/azure-pipelines/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# This build script is licensed under CC0 1.0 Universal:
+# https://creativecommons.org/publicdomain/zero/1.0/
+
+set -euo pipefail
+IFS=$'\n\t'
+
+export DIR
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$DIR/_common.sh"
+
+# Create an export templates TPZ
+cp "$AGENT_WORKFOLDER/resources/version.txt" "$ARTIFACTS_DIR/templates/version.txt"
+cd "$ARTIFACTS_DIR/"
+7z a -r -mx9 \
+    "$ARTIFACTS_DIR/templates/godot-templates-ios-macos-nightly.tpz"
+    "templates/"
+cd "$AGENT_WORKFOLDER/"
+
+# Deploy to server using SCP
+scp -r "$ARTIFACTS_DIR"/* hugo@hugo.pro:/var/www/archive.hugo.pro/builds/godot/
