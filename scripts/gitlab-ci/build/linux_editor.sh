a73621c91b05d1e8f034cfb89bd3e9f6594d21ca
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-02-01 17:54:24 +0100

    Build Windows editor and Linux binaries with LTO enabled

--- scripts/gitlab-ci/build/linux_editor.sh
+++ scripts/gitlab-ci/build/linux_editor.sh
@@ -16,7 +16,7 @@ export CXX="g++-8"
 
 # Build Linux editor
 scons platform=x11 tools=yes target=release_debug \
-      udev=yes use_static_cpp=yes \
+      udev=yes use_static_cpp=yes use_lto=yes \
       LINKFLAGS="-fuse-ld=gold" \
       "${SCONS_FLAGS[@]}"
 
